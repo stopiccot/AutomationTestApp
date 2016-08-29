0915aa8191071fb66cfdb49bfe4c39bfd3085e46
stopiccot <alexey.petruchik@gmail.com>
2016-08-29 23:37:24 +0300

    notes

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,12 +5,19 @@ require 'calabash-cucumber/calabash_steps'
 
 calabash_launcher = Calabash::Cucumber::Launcher.new
 
+app_path = "/Users/stopiccot/Library/Developer/Xcode/DerivedData/Unity-iPhone-cnrfuklrxqjyslcvogrvqymmfjlx/Build/Products/Release-iphonesimulator/AutomationTestApp.app"
+
 options = {
-  app: "Buils/iOS/AutomationTestApp.ipa",
-  uia_strategy: :preferences,
-  launch_method: :instruments,
+  app: app_path,
+  # uia_strategy: :preferences,
+  # launch_method: :instruments,
   device_target: "iPhone 6 (9.3)",
   device: "iPhone 6 (9.3)"
 }
+
+# xcrun simctl launch booted com.one1eleven.AutomationTestApp
+# xcrun instruments -w 306845D4-622F-4ABE-A63D-B9E907CD124D -t Automation com.one1eleven.AutomationTestApp
+# ENV["APP"] = app_path
+ENV["APP_BUNDLE_PATH"] = app_path
 
 calabash_launcher.relaunch(options)
